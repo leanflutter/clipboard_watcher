872d9a6fa1373fb9dfb46bc3f44edc0488c2df08
李健莹 <lijy91@foxmail.com>
2023-02-05 23:53:20 +0800

    Bump flutter to 3.7.1 & Add lint workflow

--- test/clipboard_watcher_test.dart
+++ test/clipboard_watcher_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:clipboard_watcher/clipboard_watcher.dart';
+// import 'package:clipboard_watcher/clipboard_watcher.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('clipboard_watcher');
